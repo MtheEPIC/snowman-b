fea73ac2a74f6fd447b13290c90cdd52beb53853
MtheEPIC <mtheepic1423@protonmail.com>
2021-03-17 17:11:58 +0200

    func def

--- snowman.hpp
+++ snowman.hpp
@@ -3,5 +3,11 @@
 using namespace std;
 
 namespace ariel {
-    string snowman(int);
+    string snowman(int); // main func to create snowman
+	void add_hat(); // creates hat
+	void add_face(); // creates nose and eyes
+	void add_torso(); // creates torso
+	void add_left_arm(); // called by 'add_torso' and add arms if needed
+	void add_right_arm(); // called by 'add_torso' and add arms if needed
+	void add_base(); // creates base
 }
